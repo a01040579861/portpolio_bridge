e9bd08626f09a0af7c330742074cbe923c462515
a01040579861 <a01040579861@gmail.com>
2025-07-08 00:04:59 +0900

    next_config update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   productionBrowserSourceMaps: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
